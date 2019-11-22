1a520b2532bd61306dad7c4c2cc1aece03039aa8
cs_apple_04 <cs_apple_04@csapple04simac.aamu.edu>
2019-11-21 23:49:31 -0600

    partstagram part 1

---
+++ Parstagram2/PostCell.swift
@@ -0,0 +1,30 @@
+//
+//  PostCell.swift
+//  Parstagram2
+//
+//  Created by cs_apple_04 on 11/21/19.
+//  Copyright © 2019 codepath. All rights reserved.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+
+    @IBOutlet weak var photoView: UIImageView!
+    
+    @IBOutlet weak var usernameLabel: UILabel!
+    
+    @IBOutlet weak var captionLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
